e78ac0242a59b00730fb93f4e6f39eb9e37a802c
Aditya019 <adityapande019@gmail.com>
2026-06-09 20:09:32 +0530

    feat: implemented user registration and login auth

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import {connectDb} from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+
+
 
 dotenv.config();
 
@@ -12,6 +15,7 @@ connectDb();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth',authRoutes);
 
 app.get('/api/test',(req,res)=>{
     res.json({
